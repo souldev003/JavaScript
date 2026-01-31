781ade12115c8bef236c86275658346f51b58445
souldev003 <mehrabhosen003@gmail.com>
2026-01-31 19:27:04 +0600

    argument

---
+++ Basics/Function/parametar.js
@@ -0,0 +1,14 @@
+function study(Book) {
+  console.log("open your bag");
+  console.log("take your book on your table");
+  console.log("open your book,,,, then study.");
+}
+
+study(5);
+
+function square(num) {
+  sum = num * num;
+  console.log(sum);
+}
+
+square(405); // we can call function anytime by needs.
