55039e4544b82b722ae8137f3dabe24a7f179663
souldev003 <mehrabhosen003@gmail.com>
2026-01-31 20:15:12 +0600

    function return

---
+++ Basics/Function/argument.js
@@ -0,0 +1,11 @@
+let fatherAge = 55;
+let myAge = 23;
+
+function difference(num1, num2) {
+  let diff = num1 - num2;
+  console.log(
+    `First value is: ${num1} Second value is: ${num2}. Difference is: ${diff}`,
+  );
+}
+
+difference(fatherAge, myAge);
---
+++ Basics/Function/returns.js
@@ -0,0 +1,12 @@
+function TenX(number) {
+  const result = number * 10;
+  return result;
+}
+
+function Half(number) {
+  const result = number / 2;
+  return result;
+}
+
+let numbers = TenX(100);
+console.log(numbers);
