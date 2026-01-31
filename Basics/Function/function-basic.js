5f0e15071611e5cd0593301eaccd146d88a54d61
souldev003 <mehrabhosen003@gmail.com>
2026-01-31 19:09:03 +0600

    parametar

---
+++ Basics/Function/function-basic.js
@@ -0,0 +1,13 @@
+const price = 2550;
+let offer = 10;
+
+// declare the function
+function offerValue() {
+  console.log("hello javaScript");
+  console.log("hello javaScript");
+  console.log("hello javaScript");
+  console.log("hello javaScript");
+  console.log("hello javaScript");
+}
+// call the function
+offerValue();
