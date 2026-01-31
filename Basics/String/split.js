31b712809c409e1439cd205179945f444765e338
souldev003 <mehrabhosen003@gmail.com>
2026-01-31 11:49:28 +0600

    obejct

---
+++ Basics/String/split.js
@@ -0,0 +1,21 @@
+// slice
+// const address = "Gopalganj";
+
+// let part = address.slice(5); // 4 index theke ses porjonto dekhabe //ganj
+// console.log(part);
+// console.log(address); // eta main element er value change korbe na, notun ekta alada string banabe.
+//
+//
+//
+// const address = "Gopalganj";
+// let word = address.split(" "); // we can make string to array using split.
+// console.log(word);
+
+let name = "Rohim,Karim,Abdullah,Abdur Rahman";
+let realFriend = name.split(",");
+
+console.log(realFriend);
+console.log(typeof realFriend);
+
+let friends = ["Rohim", "Karim", "Abdullah", "Abdur Rahman"];
+console.log(typeof friends);
