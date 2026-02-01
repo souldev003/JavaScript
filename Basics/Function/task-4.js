7d78ebf8d24bfffd308fbc8c1add8d6e5301f6d1
souldev003 <mehrabhosen003@gmail.com>
2026-02-01 20:34:11 +0600

    task done

---
+++ Basics/Function/task-4.js
@@ -0,0 +1,16 @@
+/**
+ * Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+ */
+
+function count_zero(str, num) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === num) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let countZero = count_zero("01110100011110001010101", "0");
+console.log(countZero);
