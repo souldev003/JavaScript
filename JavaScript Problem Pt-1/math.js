e33ccd40238b280a3b0bb5a4b3b20b37cbc6ecbe
souldev003 <mehrabhosen003@gmail.com>
2026-02-02 16:24:15 +0600

    math.js

---
+++ JavaScript Problem Pt-1/math.js
@@ -0,0 +1,19 @@
+// const math = {
+//   min: function min(num1, num2) {
+//     return num2;
+//   },
+// };
+
+const min = Math.min(45, 21, 56, 23, 45, 57, 21);
+console.log(min);
+const max = Math.max(45, 21, 56, 23, 45, 57, 21);
+console.log(max);
+
+console.log(Math.PI.toFixed(3));
+console.log(Math.abs(-525 - 100));
+console.log(Math.round(3.46));
+console.log(Math.ceil(53.85));
+console.log(Math.random() * 10);
+
+let num = Math.round(Math.random() * 10); // 1 theke 10 porjonto number pabo
+console.log(num);
