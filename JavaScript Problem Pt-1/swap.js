5a6f9949d387e1114b0856ba923c55508c1628e9
souldev003 <mehrabhosen003@gmail.com>
2026-02-02 20:34:23 +0600

    javaScript part-1 done

---
+++ JavaScript Problem Pt-1/swap.js
@@ -0,0 +1,32 @@
+// simple way
+// let a = 5;
+// let b = 7;
+// console.log(a, b);
+// let temp = a;
+
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+// let x = 5;
+// let y = 7;
+// console.log(x, y);
+
+// [x, y] = [y, x];
+// console.log(x, y);
+
+// console.log(Math.pow(3, 3));
+console.log(Math.ceil(0.95));
+
+console.log(Math.ceil(7.004));
+// Expected output: 8
+
+console.log(Math.ceil(-7.004));
+
+console.log(Math.sqrt(144));
+
+for (var i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
