0cf3af6c80ea3806e9f84b3a6b0f13282c539fc1
souldev003 <mehrabhosen003@gmail.com>
2026-02-03 10:48:38 +0600

    wood quantity problem solved

---
+++ JavaScript Problem Pt-2/wood.js
@@ -0,0 +1,23 @@
+/**
+ * Chair ---> 3 CFT
+ * Table ---> 10 CFT
+ * Bed -----> 50 CFT
+ */
+
+function woodQuantity(chairQuantity, tableQuantity, bedQuantity) {
+  const perChairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
+
+  const woodForChair = chairQuantity * perChairWood;
+  const woodForTable = tableQuantity * perTableWood;
+  const woodForBed = bedQuantity * perBedWood;
+
+  const totalWoodCost = woodForChair + woodForTable + woodForBed;
+
+  return `Wood needed for ${chairQuantity} Chair: ${woodForChair} CFT. Wood Needed for ${tableQuantity} Table: ${woodForTable} CFT. wood needed for ${bedQuantity} Bed: ${woodForBed} CFT. Total wood cost for all item: ${totalWoodCost} CFT.
+  `;
+}
+
+let roomDecoration = woodQuantity(2, 2, 1);
+console.log(roomDecoration);
