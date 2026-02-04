14b87193571ea4ae4a96945cdce86f3d139f2188
souldev003 <mehrabhosen003@gmail.com>
2026-02-04 20:15:45 +0600

    task 4 started

---
+++ JavaScript Problem Pt-1/task-4.js
@@ -0,0 +1,16 @@
+//Write a function to find the longest word in a given string.
+
+// sample-input: I am learning Programming to become a programmer
+
+// sample-output: Programming
+
+function getLongestWord(sentence, wordYouNeed) {
+  let word = 0;
+  for (let word of sentence) {
+  }
+}
+
+let sentence =
+  "I am learning programming to become a programmer. I like to solve problem using JavaScript.";
+
+let findLongestWord = getLongestWord(sentence);
