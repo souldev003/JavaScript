e12974baf7db8b6721e93face16f2b7e7462cc86
souldev003 <mehrabhosen003@gmail.com>
2026-02-05 21:10:19 +0600

    task-5 completed

---
+++ JavaScript Problem Pt-2/task-4.js
@@ -0,0 +1,37 @@
+/* You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+
+Input
+
+
+    const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ]; */
+
+function findAveragePhonePrice(phones) {
+  let allPhonePrice = 0;
+  let count = 0;
+  for (let phone of phones) {
+    count = count + 1;
+    allPhonePrice += phone.price;
+  }
+  //   return allPhonePrice;
+  const averagePrice = allPhonePrice / phones.length;
+  return averagePrice;
+}
+
+const phones = [
+  { model: "PhoneA", brand: "Iphone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+const averagePrice = findAveragePhonePrice(phones);
+console.log("Average Price of all phone is:", averagePrice.toFixed(2), "Taka");
---
+++ JavaScript Problem Pt-2/task-5.js
@@ -0,0 +1,59 @@
+/* For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+
+const employees = [
+           { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+           { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+           { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+           { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+       ]; */
+
+function salaryCalculator(employees) {
+  let perMonthSalary = 0;
+  for (let employee of employees) {
+    const singlePersonPerMonthSalary =
+      employee.experience * employee.increment + employee.starting;
+    perMonthSalary += singlePersonPerMonthSalary;
+  }
+  return perMonthSalary;
+}
+
+/**Optional Getting highest paid employee salary*/
+
+function getHighestPaidEmployee(employees) {
+  let highestPaidEmployee = employees[0];
+  for (let employee of employees) {
+    const regularEmployeeSalary =
+      employee.increment * employee.experience + employee.starting;
+
+    const highestPaidEmployeeSalary =
+      highestPaidEmployee.increment * highestPaidEmployee.experience +
+      highestPaidEmployee.starting;
+
+    if (regularEmployeeSalary > highestPaidEmployeeSalary) {
+      highestPaidEmployee = employee;
+    }
+  }
+  const totalSalaryHighestPaid =
+    highestPaidEmployee.increment * highestPaidEmployee.experience +
+    highestPaidEmployee.starting;
+  return {
+    ...highestPaidEmployee,
+    totalSalaryHighestPaid: totalSalaryHighestPaid,
+  };
+}
+
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+const highestPaidEmployeeSalary = getHighestPaidEmployee(employees);
+console.log("The highest paid employee salary is:", highestPaidEmployeeSalary);
+
+let totalSalaryEachMonth = salaryCalculator(employees);
+console.log(
+  "Total Salary For All Employees per month is:",
+  totalSalaryEachMonth,
+);
